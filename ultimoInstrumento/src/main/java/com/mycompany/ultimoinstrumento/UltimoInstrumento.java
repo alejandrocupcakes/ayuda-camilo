b4c020c85af4b7061171e652c9ac4b03c58807ae
alejandrocupcakes <148361406+alejandrocupcakes@users.noreply.github.com>
2024-06-18 13:32:03 -0500

    Update UltimoInstrumento.java

--- ultimoInstrumento/src/main/java/com/mycompany/ultimoinstrumento/UltimoInstrumento.java
+++ ultimoInstrumento/src/main/java/com/mycompany/ultimoinstrumento/UltimoInstrumento.java
@@ -5,46 +5,46 @@ import javax.swing.JOptionPane;
 
 public class UltimoInstrumento {
     public static void main(String[] args) {
-        int estudiantes = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de estudiantes")), aprovados=0, reprovados=0, puedenRecuperar=0;
-        ArrayList<ArrayList<Double>> notas = new ArrayList<ArrayList<Double>>();
-        ArrayList<Double> promedios = new ArrayList<Double>(), arrayTemporal = new ArrayList<Double>();
-        for (int i = 1; i <= estudiantes; i++) {
-            arrayTemporal.clear();
+        int Estudiante = Integer.parseInt(JOptionPane.showInputDialog("Ingresa la cantidad de estudiantes")), aprovados=0, reprovados=0, recuperar=0;
+        ArrayList<ArrayList<Double>> nota = new ArrayList<ArrayList<Double>>();
+        ArrayList<Double> promedios = new ArrayList<Double>(), arrayX = new ArrayList<Double>();
+        for (int i = 1; i <= Estudiante; i++) {
+            arrayX.clear();
             for (int j = 1; j <= 3; j++) {
-                double nota = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la nota "+j+" del estudiante numero "+i));
-                if (nota >= 0 && nota <= 5) {
-                    arrayTemporal.add(nota);
+                double notas = Double.parseDouble(JOptionPane.showInputDialog("Ingresar la nota "+j+" del estudiante: "+i));
+                if (notas >= 0 && notas <= 5) {
+                    arrayX.add(notas);
                 } else {
-                    JOptionPane.showMessageDialog(null, "La nota debe estar en un rango valido (1-5)");
+                    JOptionPane.showMessageDialog(null, "La nota debe ser entre 1 y 5");
                     System.exit(0);
                 }
             }
-            notas.add(arrayTemporal);
-            promedios.add((arrayTemporal.get(0) + arrayTemporal.get(1) + arrayTemporal.get(2)) / 3);
+            nota.add(arrayX);
+            promedios.add((arrayX.get(0) + arrayX.get(1) + arrayX.get(2)) / 3);
         }
-        JOptionPane.showMessageDialog(null, "Se han validado "+estudiantes+" estudiantes");
-        JOptionPane.showMessageDialog(null, "Se han ingresado "+(notas.size()*3)+" notas");
-        for (int i = 1; i <= estudiantes; i++) {
+        JOptionPane.showMessageDialog(null, "Se han validado "+Estudiante+" estudiantes");
+        JOptionPane.showMessageDialog(null, "Se han ingresado "+(nota.size()*3)+" notas");
+        for (int i = 1; i <= Estudiante; i++) {
             if (promedios.get(i - 1) >= 3.5)
-                aprovados += 1;
+                aprovado += 1;
             else
-                reprovados += 1;
+                reprovado += 1;
             if (promedios.get(i - 1) >= 2)
-                puedenRecuperar += 1;
+            recuperar += 1;
         }
-        JOptionPane.showMessageDialog(null, aprovados + " estudiantes han aprovado");
-        JOptionPane.showMessageDialog(null, reprovados + " estudiantes han reprovado");
-        JOptionPane.showMessageDialog(null, puedenRecuperar + " estudiantes pueden recuperar");
-        JOptionPane.showMessageDialog(null, (estudiantes - puedenRecuperar) + " estudiantes no pueden recuperar");
+        JOptionPane.showMessageDialog(null, aprovado + " estudiantes han aprovado");
+        JOptionPane.showMessageDialog(null, reprovado + " estudiantes han reprovado");
+        JOptionPane.showMessageDialog(null, recuperar + " estudiantes pueden recuperar");
+        JOptionPane.showMessageDialog(null, (Estudiante - recuperar) + " estudiantes no pueden hacer recuperacion");
         String lista = "";
-        int promedioFinal = 0;
-        for (int i = 0; i < estudiantes; i++) {
-            promedioFinal += promedios.get(i);
+        int PromedioFinal = 0;
+        for (int i = 0; i < Estudiante; i++) {
+            PromedioFinal += promedio.get(i);
         }
-        promedioFinal = promedioFinal / estudiantes;
-        JOptionPane.showMessageDialog(null, "El promedio de todas las notas es "+promedioFinal);
-        for (int i = 0; i < estudiantes; i++) {
-            lista += ("El estudiante" + i + " ha sacado un promedio de "+promedios.get(i)+"\n"); 
+        PromedioFinal = PromedioFinal / Estudiante;
+        JOptionPane.showMessageDialog(null, "El promedio toral de las nota son: "+PromedioFinal);
+        for (int i = 0; i < Estudiante; i++) {
+            lista += ("El estudiante" + i + " ha sacado un promedio de "+promedio.get(i)+"\n"); 
         }
         JOptionPane.showMessageDialog(null, lista);
     }
